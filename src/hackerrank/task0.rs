3b5a29fc3aa9fbcabee097859e11a2e12b57d6ee
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2026-02-19 18:51:42 +0200

    fix naming, results added

--- src/hackerrank/task0.rs
+++ src/hackerrank/task0.rs
@@ -1,5 +1,5 @@
 // https://www.hackerrank.com/challenges/simple-array-sum/problem
-fn simpleArraySum(aa: &[i32]) -> i32 {
+fn simple_array_sum(aa: &[i32]) -> i32 {
     let mut x: i32 = 0;
     for a in aa {
         x += a
@@ -10,7 +10,7 @@ fn simpleArraySum(aa: &[i32]) -> i32 {
 #[test]
 fn test0() {
     let aa = vec![10, 11, 12];
-    let real = simpleArraySum(&aa);
+    let real = simple_array_sum(&aa);
     let expected = 10 + 11 + 12;
     assert_eq!(real, expected);
 }
